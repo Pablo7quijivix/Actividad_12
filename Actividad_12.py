91761caaa30803c8eb8d7fa5f54f6459475e13e0
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-08-04 19:05:56 -0600

    2 commit en clase

--- Actividad_12.py
+++ Actividad_12.py
@@ -1,18 +1,47 @@
 
 print(f"=====PROMEDIO DE ESTUDIANTES=====")
-try:
-    notas =[]
-    students = int(input("===¿CUANTOS ESTUDIANTES DESEA INGRESAR===: ?"))
-for i in range(students):
-    print(f"___INGRESE ESTUDIANTES {i+1}")
-    name = input(f"___INGRESE NOMBRE DEL ESTUDIANTE___: ")
-    cantidad_notas = int(input(f"___¿CUANTAS NOTAS DESEA INGRESAR___: ?"))
-    for j in range(cantidad_notas):
-        notes= int(input(f"___===INGRESE LA NOTA.{j+1}===: "))
-        notas.append(notes)
-        promedio = sum(notas) / len(notas)
-print(f"=====ESTUDIANTE: {name}__PROMEDIO DEL ESTUDIANTE: {promedio}")
+notas={}
+while True:
+    try:
 
-except ValueError:
-    print("Error: Debes ingresar números válidos.")
+        while True:
+            #pedimos la cantidad de estudiantes que queremos
+            students = int(input("===¿CUANTOS ESTUDIANTES DESEA INGRESAR===?: "))
+            #iteramos para recopilar cada dato del estudiante: nombre, cantidad de notas que desea, las notas
+            for i in range(students):
+                print(f"\n___INGRESE ESTUDIANTES {i+1}")
+                name = input(f"___INGRESE NOMBRE DEL ESTUDIANTE___: ")
+                cantidad_notas = int(input(f"___¿CUANTAS NOTAS DESEA INGRESAR PARA EL ESTUDIANTE: {name}___: ?"))
+
+                #lista para guardar las notas de este estudiante
+                notas_2= []
+                for j in range(cantidad_notas):
+                    notes=int(input(f"\n___INGRESE LA NOTA: {j+1} DEL ESTUDIANTE:{name}____: "))
+                    notas_2.append(notes)
+                    #estamos guardando la lista dentro del diccionario
+                    notas['name'] = notas_2
+                    print(f"la nota es {notas['name'][2]}")
+                    promedio = sum(notas_2) / len(notas_2)
+                    print(promedio)
+    except ValueError:
+        print(f"Error: Debes ingresar numeros validos.")
+
+    except ZeroDivisionError:
+        print(f"Error: no se puede dividir dentro de cero.")
+
+    except TypeError:
+        print(f"No puedes combinar 2 tipos de datos diferentes (texto + numero).")
+
+    except Exception as e:
+        print("Se produjo un error inesperado:",e)
+
+    else:
+        print(f"====EL RESULTADO DEL PROMEDIO DEL ESTUDIANTE: {name} ES IGUAL A:{notas[promedio]} ====")
+
+    finally:
+        print("Fin del proceso.")
+
+
+#print(f"=====ESTUDIANTE: {name}__PROMEDIO DEL ESTUDIANTE: {promedio}")
+
 
